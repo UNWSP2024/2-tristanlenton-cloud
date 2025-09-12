132ffacf52291cc48d434aa25509d34e69cffae1
tristanlenton-cloud <tristan.lenton@gmail.com>
2025-09-12 09:00:46 -0500

    Add files via upload

--- program_3.py
+++ program_3.py
@@ -1,8 +1,8 @@
-item1 = int(input("Item 1: "))
-item2 = int(input("Item 2: "))
-item3 = int(input("Item 3: "))
-item4 = int(input("Item 4: "))
-item5 = int(input("Item 5: "))
+item1 = float(input("Item 1: "))
+item2 = float(input("Item 2: "))
+item3 = float(input("Item 3: "))
+item4 = float(input("Item 4: "))
+item5 = float(input("Item 5: "))
 subtotal = (item1 + item2 + item3 + item4 + item5)
 tax = (item1 + item2 + item3 + item4 + item5) * 0.07
 print("Subtotal:", subtotal)
